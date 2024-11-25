feaf269776baa8b04adc833413e7537b664b575b
Rayani Gomes <rayanigomesrn100@gmail.com>
2024-11-25 17:24:38 -0300

    fix: implementando a api chamando a categoria do filtro

--- src/(components)/filtro/categoriasDropdown/index.tsx
+++ src/(components)/filtro/categoriasDropdown/index.tsx
@@ -1,14 +1,26 @@
+import { CategoriaProps } from '@/interfaces';
+import api from '@/service/api';
+import { useEffect, useState } from 'react';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function CategoriaDropdown() {
+    const [categorias, setCategorias] = useState<CategoriaProps[]>([]);
+
+    useEffect(() => {
+        api.get('/categories')
+            .then((res) => res.data)
+            .then((data) => setCategorias(data));
+    }, []);
+    
     return (
         <>
             <NavDropdown
                 id="nav-dropdown-dark-example"
                 title="Categorias"
             >
-                <NavDropdown.Item href="#action/3.1">Categoria</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.1">Categoria</NavDropdown.Item>
+                {categorias.map((categoria) => (
+                    <NavDropdown.Item href="#" key={categoria.id}>{categoria.name}</NavDropdown.Item>
+                ))}
             </NavDropdown>
         </>
     )
