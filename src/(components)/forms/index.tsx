737426c54a79c34fac02438f616e6fa897a5d2e5
Rayani Gomes <rayanigomesrn100@gmail.com>
2024-11-28 18:02:05 -0300

    fix: cadastramento de produtos na api

--- src/(components)/forms/index.tsx
+++ src/(components)/forms/index.tsx
@@ -2,13 +2,16 @@ import { useEffect, useState } from "react";
 import { CampoButtons, CampoTexto, CampoTextoDuplo, ContainerForms, Quantidade } from "./styled";
 import { CategoriaProps, PropProdutos } from "@/interfaces";
 import api from "@/service/api";
-import { formatter } from "../tabelas/movimentacoes";
 
 interface PropsDataProduto {
     produto?: PropProdutos
 }
 
-export default function Forms({produto = {} as PropProdutos}: PropsDataProduto) {
+export default function Forms({ produto = {} as PropProdutos }: PropsDataProduto) {
+    const [nomeProduto, setNomeProduto] = useState<PropProdutos>({} as PropProdutos);
+    const [precoProduto, setPrecoProduto] = useState<PropProdutos>({} as PropProdutos);
+    const [descricaoProduto, setDescricaoProduto] = useState<PropProdutos>({} as PropProdutos);
+
     const [quantidade, setQuantidade] = useState(1);
     const [InfoCategoria, setInfoCategoria] = useState<CategoriaProps[]>([]);
 
@@ -32,19 +35,47 @@ export default function Forms({produto = {} as PropProdutos}: PropsDataProduto) 
         }
     };
 
+    const criarProduto = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        console.log(nomeProduto, precoProduto, quantidade, descricaoProduto);
+
+        const novoProduto = {
+            name: nomeProduto.name,
+            description: descricaoProduto.description,
+            price: precoProduto.price,
+            stock_quantity: quantidade,
+            category: produto.category
+        }
+        api.post('/products/', novoProduto)
+        .then((res) => {
+            console.log(res.data);
+        })
+            .catch((err) => console.log(err));
+    }
+
     return (
-        <ContainerForms>
+        <ContainerForms onSubmit={(e) => criarProduto(e)}>
             <CampoTexto>
                 <label>Nome:</label>
-                <input type="text" placeholder="Nome do produto" value={produto.name} />
+                <input
+                    type="text"
+                    placeholder="Nome do produto"
+                    defaultValue={produto.name}
+                    onChange={(e) => setNomeProduto({ ...nomeProduto, name: e.target.value })}
+                />
             </CampoTexto>
             <CampoTextoDuplo>
                 <CampoTexto>
                     <label>Categoria:</label>
-                    <select name="" id="">
-                        <option value={produto?.category?.id}>{produto?.category?.name}</option>
+                    <select
+                        defaultValue={produto?.category?.id}                                                         
+                    >
+                        <option defaultValue={produto?.category?.id}>{produto?.category?.name}</option>
                         {InfoCategoria.map((categoria) => (
-                            <option key={categoria.id}>{categoria.name}</option>
+                            <option key={categoria.id} defaultValue={categoria.id}>
+                                {categoria.name}
+                            </option>
                         ))}
                     </select>
                 </CampoTexto>
@@ -52,7 +83,11 @@ export default function Forms({produto = {} as PropProdutos}: PropsDataProduto) 
             <CampoTextoDuplo>
                 <CampoTexto>
                     <label htmlFor="">Preço:</label>
-                    <input type="text" value={formatter.format(produto.price)} />
+                    <input
+                        type="number"
+                        defaultValue={produto.price}
+                        onChange={(e) => setPrecoProduto({ ...precoProduto, price: parseFloat(e.target.value) })}
+                    />
                 </CampoTexto>
                 <CampoTexto>
                     <label htmlFor="">Quantidade:</label>
@@ -60,9 +95,9 @@ export default function Forms({produto = {} as PropProdutos}: PropsDataProduto) 
                         <button type="button" className="bi bi-dash" onClick={diminuirQuantidade} aria-label="Diminuir quantidade" />
                         <input
                             type="text"
-                            value={produto.stock_quantity}
-                            readOnly
+                            defaultValue={produto.stock_quantity}
                             min="1"
+                            placeholder="1"
                         />
                         <button type="button" className="bi bi-plus" onClick={aumentarQuantidade} aria-label="Aumentar quantidade" />
                     </Quantidade>
@@ -70,7 +105,10 @@ export default function Forms({produto = {} as PropProdutos}: PropsDataProduto) 
             </CampoTextoDuplo>
             <CampoTexto>
                 <label htmlFor="">Descrição:</label>
-                <textarea name="" id="" value={produto.description} />
+                <textarea
+                    defaultValue={produto.description}
+                    onChange={(e) => setDescricaoProduto({ ...descricaoProduto, description: e.target.value })}
+                />
             </CampoTexto>
             <CampoButtons>
                 <button className="bntPrincipal">Cadastrar</button>
