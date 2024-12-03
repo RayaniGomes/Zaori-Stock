a9e2cf1f5a4bff24fae386f9debde5e201862979
Rayani Gomes <rayanigomesrn100@gmail.com>
2024-12-03 18:05:04 -0300

    fix: contador funcinando

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,17 +5,49 @@ import Contador from "@/(components)/contador";
 import Filtro from "@/(components)/filtro";
 import Tabela from "@/(components)/tabelas/movimentacoes";
 import { Container, ContainerMain, Main } from "../styled";
+import api from "@/service/api";
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [contProdutos, setContProdutos] = useState(0);
+  const [contEntradas, setContEntradas] = useState(0);
+  const [contSaidas, setContSaidas] = useState(0);
+
+  useEffect(() => {
+    api.get("/products/")
+      .then(response => {
+        setContProdutos(response.data.length);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+      api.get("movements/?movement_type=IN")
+      .then(response => {
+        setContEntradas(response.data.length);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+      api.get("movements/?movement_type=OUT")
+      .then(response => {
+        setContSaidas(response.data.length);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <Container>
       <Navbar />
       <Main>
         <ContainerMain>
           <SectionContador>
-            <Contador title="Produtos no estoque" cont={1000} />
-            <Contador title="Produtos no estoque" cont={1000} />
-            <Contador title="Produtos no estoque" cont={1000} />
+            <Contador title="Produtos no estoque" cont={contProdutos} />
+            <Contador title="Entradas de produtos" cont={contEntradas} />
+            <Contador title="Saidas de produtos" cont={contSaidas} />
           </SectionContador>
           <Filtro />
           <h3>Últimas movimentações</h3>
